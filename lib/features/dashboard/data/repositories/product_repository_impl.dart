3dc2f8ddf2e4e074c7448b409ebdb6feeda2440c
helenoktaa <1123150205@global.ac.id>
2026-04-18 10:44:15 +0700

    feat: add ProductRepositoryImpl
    Implementasi ProductRepository menggunakan DioClient. getProducts: GET /products dengan queryParameters page, limit, category. getProductById: GET /products/:id. Parse response ke List<ProductModel>.

--- lib/features/dashboard/data/repositories/product_repository_impl.dart
+++ lib/features/dashboard/data/repositories/product_repository_impl.dart
@@ -1,19 +1,38 @@
 import 'package:shopping_tangerang/core/constants/api_constants.dart';
 import 'package:shopping_tangerang/core/services/dio_client.dart';
-import 'package:shopping_tangerang/features/auth/domain/repositories/auth_repository.dart';
+import 'package:shopping_tangerang/features/dashboard/data/models/product_model.dart';
+import 'package:shopping_tangerang/features/dashboard/domain/repositories/product_repository.dart';
 
-class AuthRepositoryImpl implements AuthRepository {
+class ProductRepositoryImpl implements ProductRepository {
   @override
-  Future<String> verifyFirebaseToken(String firebaseToken) async {
-    final response = await DioClient.instance.post(
-      ApiConstants.verifyToken,
-      data: {'firebase_token': firebaseToken},
+  Future<List<ProductModel>> getProducts({
+    int page = 1,
+    int limit = 10,
+    String? category,
+  }) async {
+    final response = await DioClient.instance.get(
+      ApiConstants.products,
+      queryParameters: {
+        'page': page,
+        'limit': limit,
+        'category': category,
+      },
     );
 
 
-    final data = response.data['data'] as Map<String, dynamic>;
-    return data['access_token'] as String;
+    final List<dynamic> data = response.data['data'];
+    return data.map((e) => ProductModel.fromJson(e)).toList();
+  }
+
+
+  @override
+  Future<ProductModel> getProductById(int id) async {
+    final response = await DioClient.instance.get(
+      '${ApiConstants.products}/$id',
+    );
+    return ProductModel.fromJson(response.data['data']);
   }
 }
+
 
 
