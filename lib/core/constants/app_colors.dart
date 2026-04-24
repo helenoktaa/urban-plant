c2bccb927764171ba351a9ab017ec46534378786
helenoktaa <1123150205@global.ac.id>
2026-04-24 20:41:26 +0700

    menambahkan dark mode ke app_colors

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  // Brand 
   static const Color primary = Color(0xFF2E7D32);
   static const Color primaryLight = Color(0xFF4CAF50);
   static const Color primaryDark = Color(0xFF1B5E20);
   static const Color accent = Color(0xFF81C784);
 
+
+  // Light Mode
   static const Color background = Color(0xFFF5F5F0);
   static const Color surface = Colors.white;
   static const Color error = Colors.red;
@@ -16,4 +19,19 @@ class AppColors {
 
   static const Color divider = Color(0xFFE0E0E0);
   static const Color border = Color(0xFFE0E0E0);
+
+  //Dark Mode
+  static const Color darkBackground  = Color(0xFF121212); // latar halaman
+  static const Color darkSurface     = Color(0xFF1E1E1E); // AppBar, bottom nav
+  static const Color darkSurfaceCard = Color(0xFF2C2C2C); // kartu, input field
+
+
+  static const Color darkTextPrimary   = Color(0xFFEEEEEE);
+  static const Color darkTextSecondary = Color(0xFFAAAAAA);
+  static const Color darkTextHint      = Color(0xFF666666);
+
+
+  static const Color darkDivider = Color(0xFF3A3A3A);
+  static const Color darkBorder  = Color(0xFF3A3A3A);
+
 }
