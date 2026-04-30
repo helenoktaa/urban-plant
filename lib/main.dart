5928aae8b6af5bd0a13e43324a06c2e13da5530b
helenoktaa <1123150205@global.ac.id>
2026-04-30 21:14:09 +0700

    feat: integrasi dark mode ke aplikasi utama
    - menghubungkan ThemeProvider ke MaterialApp
    - menerapkan light dan dark theme
    - menggunakan context.watch untuk update UI otomatis
    - menyesuaikan routing menggunakan AppRouter.routes

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,45 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:urban_plant/core/routes/app_router.dart';
 import 'package:urban_plant/features/auth/presentation/providers/auth_provider.dart';
+import 'package:urban_plant/core/providers/theme_provider.dart';
+import 'package:urban_plant/core/theme/app_theme.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-
   // Inisialisasi Firebase SEBELUM runApp
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
- 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
       ],
       child: const MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeProvider>(
+      builder: (context, theme, _) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+
+          theme: AppTheme.light,
+          darkTheme: AppTheme.dark,
+          themeMode: theme.themeMode,
+
+          initialRoute: AppRouter.splash,
+          routes: AppRouter.routes,
+        );
+      },
+    );
+  }
 }
